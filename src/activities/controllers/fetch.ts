a61baba479b5eed1ea8eb83565f21923c18efb0f
samdouble <samleafs@protonmail.com>
2024-09-30 20:03:01 -0400

    chore: remove Google Auth-related stuff

--- src/activities/controllers/fetch.ts
+++ src/activities/controllers/fetch.ts
@@ -1,9 +1,5 @@
-import HttpStatus from 'http-status-codes';
 import Joi from 'joi';
-import { DateTime } from 'luxon';
-import regexEscape from 'regex-escape';
 import Activity from '../schemas/Activity';
-import { ExpressRouteError } from '../../utils/ExpressRouteError';
 
 const filterSchema = Joi.object().keys({
   startTime: Joi.string().isoDate(),
@@ -21,39 +17,10 @@ const schema = Joi.object().keys({
   q: Joi.string(),
 });
 
-export default async (userId, pQuery, options: { session?: any } = {}) => {
-  let validation;
-  try {
-    const jsonQuery = (typeof pQuery === 'string') ? JSON.parse(pQuery) : pQuery;
-    const jsonFilter = jsonQuery.filter && JSON.parse(jsonQuery.filter);
-    const jsonSort = jsonQuery.sort && JSON.parse(jsonQuery.sort);
-    const { q } = jsonQuery;
-    validation = schema.validate({
-      filter: jsonFilter,
-      sort: jsonSort,
-      q,
-    }, { stripUnknown: true });
-  } catch (error) {
-    throw new ExpressRouteError(HttpStatus.BAD_REQUEST, 'Invalid request');
-  }
-  if (validation.error) {
-    throw new ExpressRouteError(HttpStatus.BAD_REQUEST, 'Invalid request');
-  }
-  const query = validation.value;
-  const filter = query && query.filter;
-  const sort = query && query.sort;
-  const startTime = filter && filter.startTime;
-  const endTime = filter && filter.endTime;
-  const search = query && query.q;
+export default async (userId, options: { session?: any } = {}) => {
   return Activity
     .find({
       userId,
-      ...(endTime && { startTime: { $lte: DateTime.fromISO(endTime).toUTC().toJSDate() } }),
-      ...(startTime && { endTime: { $gte: DateTime.fromISO(startTime).toUTC().toJSDate() } }),
-      ...(search && { comments: { $regex: regexEscape(search) , $options: 'i' } }),
-    })
-    .sort({
-      ...sort,
     })
     .session(options.session)
     .exec()
