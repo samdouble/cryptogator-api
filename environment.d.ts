7e8116724d42a8e1baa414a7cfaa2ccf22507e15
samdouble <samleafs@protonmail.com>
2024-10-02 13:34:31 -0400

    Ci/use actions 2 (#6)
    * ci: use github.ref_name
    
    * ci: add commitlint

--- environment.d.ts
+++ environment.d.ts
@@ -2,6 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       BASE_URL: string;
+      JWT_SECRET: string;
       MONGO_URL: string;
       NODE_ENV: 'development' | 'production';
     }
